90e82ba55b13d68e67006eea3754cafd1080379c
corndogit <nathanfinlaybrenton@gmail.com>
2022-06-07 15:24:24 +0100

    create: check environment variables for API key/secret and replace the API key/secret imported from config.json
    bugfix: force figure axis spines to stay hidden

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-# from dotenv import load_dotenv
+import os
+from dotenv import load_dotenv
 # from weatherlib import weather
 from datetime import datetime
 import numpy as np
@@ -11,15 +12,19 @@ from colour import Color
 def main(config):
     # Separate config.json
     curve_config = config["hilbertCurveProperties"]
-    # weather_config = config['weatherDataProperties']
+    weather_config = config['weatherDataProperties']
+
+    load_dotenv()  # dev tool: replaces config.json keys with env keys
+    if os.environ["DATAHUB_API_KEY"] and os.environ["DATAHUB_SECRET"]:
+        weather_config["dataHubAPIKey"] = os.getenv("DATAHUB_API_KEY")
+        weather_config["dataHubSecret"] = os.getenv("DATAHUB_SECRET")
 
     # Fetch weather data from Met Office for Arts Arkade
-    # load_dotenv()
     # weather.get_weather(weather_config)
 
     # Generate Hilbert curve segments
     n = 2
-    p = 6  # warning: this increases the number of points (and memory requirements) exponentially!!
+    p = 3  # warning: this increases the number of points (and memory requirements) exponentially!!
     hilbert_curve = HilbertCurve(p, n)
     distances = np.arange(2**(p*n))
     full_distance = len(distances)
@@ -31,8 +36,9 @@ def main(config):
     colour_range = np.fromiter(start_colour.range_to(end_colour, full_distance), dtype='S16', count=full_distance)
 
     # Configure subplot, background formatting
-    # | Removes all axis labels, forces 1:1 aspect ratio
-    fig = plt.figure()
+    # | Removes all axis labels, forces 1:1 aspect ratio, sets size
+    px = 1 / plt.rcParams['figure.dpi']
+    fig = plt.figure(figsize=(1000 * px, 1000 * px))
     ax = fig.add_subplot()
     ax.set_aspect('equal', adjustable='box')
     ax.set_facecolor('#333333')  # todo use weather data to change figure bg colour
@@ -40,6 +46,8 @@ def main(config):
                     bottom=False,
                     labelleft=False,
                     labelbottom=False)
+    for side in ['top', 'left', 'bottom', 'right']:
+        ax.spines[side].set_visible(False)
 
     # plot segments
     count = 0
@@ -56,8 +64,10 @@ def main(config):
     if curve_config["saveFigToFile"]:
         filename = datetime.now()
         file_format: str = curve_config["outputFormat"]  # allowed formats: png, svg, pdf
-        plt.savefig(f"generated_figures\\fig-{filename.strftime('%d%m%Y_%H%M%S')}.{file_format}",
-                    format=file_format)
+        plt.savefig(f"generated_figures/fig-{filename.strftime('%d%m%Y_%H%M%S')}.{file_format}",
+                    format=file_format,
+                    bbox_inches='tight',
+                    pad_inches=0)
 
 
 if __name__ == '__main__':
