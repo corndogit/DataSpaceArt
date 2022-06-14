17d55ea62a4425fc6bf0001c323f456bfb79389a
corndogit <nathanfinlaybrenton@gmail.com>
2022-06-14 19:24:02 +0100

    add: weathercodes.py linking SignificantWeatherCode to colours for colormap

---
+++ DataSpaceArt_main/weathercodes.py
@@ -0,0 +1,45 @@
+# tuples of hex colors for passing into Color objects to create colormaps
+
+clear = ('#5c92cb', '#b3f8fb')
+cloudy = ('#9a9e8f', '#f2f2f2')
+mist = ('#7fa0c3', '#a9aaab')
+light_rain = ('#4b529e', '#b8c4ef')
+heavy_rain = ('#2217ed', '#00b2ff')
+sleet_hail = ('#eaeaea', '#5aa4c4')
+snow = ('#66dbff', '#f4f4f4')
+thunder = ('#fff95b', '#e138ff')
+
+code_to_colours = {
+    'NA': clear,
+    '0': clear,
+    '1': clear,
+    '2': clear,
+    '3': clear,
+    '4': clear,  # not used
+    '5': mist,
+    '6': mist,
+    '7': cloudy,
+    '8': cloudy,
+    '9': light_rain,
+    '10': light_rain,
+    '11': light_rain,
+    '12': light_rain,
+    '13': heavy_rain,
+    '14': heavy_rain,
+    '15': heavy_rain,
+    '16': sleet_hail,
+    '17': sleet_hail,
+    '18': sleet_hail,
+    '19': sleet_hail,
+    '20': sleet_hail,
+    '21': sleet_hail,
+    '22': snow,
+    '23': snow,
+    '24': snow,
+    '25': snow,
+    '26': snow,
+    '27': snow,
+    '28': thunder,
+    '29': thunder,
+    '30': thunder
+}
