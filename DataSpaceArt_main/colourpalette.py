57f9aa95c36c286a3d4436724c6516fcb9503560
corndogit <nathanfinlaybrenton@gmail.com>
2022-06-14 19:25:39 +0100

    add: bg_color_list() creates list of rgb values when given a weather code

--- DataSpaceArt_main/colourpalette.py
+++ DataSpaceArt_main/colourpalette.py
@@ -1,9 +1,11 @@
 from colour import Color
 from math import floor
 import numpy as np
+import weathercodes
 
 
-def get_p_from_windspeed(windspeed):
+def get_p_from_windspeed(windspeed: float or int):
+    """Returns a value for the order of the Hilbert curve from natural log of the wind speed. Ranges from 3 to 8"""
     if windspeed == 0:
         return 3
     p = floor(np.log(windspeed) + 2)
@@ -11,6 +13,7 @@ def get_p_from_windspeed(windspeed):
 
 
 def line_temperature(temperature: int):
+    """Finds a colour hex value for a specific temperature, returns a string."""
     min_temp = -10
     max_temp = 35
     colours = list(Color('#11e').range_to('#e11', max_temp - min_temp + 1))
@@ -24,7 +27,15 @@ def line_temperature(temperature: int):
                 return str(v)
 
 
-def bg_direction(direction):
+def bg_color_list(weathercode: str):
+    """Returns a list of RGB values to use with ListedColormap"""
+    colours = weathercodes.code_to_colours[weathercode]
+    start_bg_colour = Color(colours[0])
+    return [colr.rgb for colr in list(start_bg_colour.range_to(colours[1], 512))]
+
+
+def bg_direction(direction: int):
+    """Calculates a direction to begin gradient from. Returns a 3x3 scalar image to use with plt.imshow()"""
     shape = np.zeros((3, 3))
     coords = {
         'N': (0, 1),
@@ -46,5 +57,7 @@ def bg_direction(direction):
 
 
 if __name__ == '__main__':
-    temp = input("Please specify a temperature:\n")
-    print(f"{temp} corresponds to value {line_temperature(int(temp))}")
+    print("get_p_from_windspeed(windspeed: float or int)\n22mph wind = ", get_p_from_windspeed(22))
+    print("line_temperature(temperature: int)\n22C = ", line_temperature(22))
+    print(f"bg_color_list(weathercode: str)\nSignificant Weather Code 1 = {bg_color_list('1')[0:8]}...")
+    print("bg_direction(direction: int) with Bearing 220 = \n", bg_direction(220))
