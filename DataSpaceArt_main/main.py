d6387491cee73ece9d002568709065e927c169d1
corndogit <nathanfinlaybrenton@gmail.com>
2022-06-15 17:55:07 +0100

    chg: pattern now generates entirely from weather data

--- DataSpaceArt_main/main.py
+++ DataSpaceArt_main/main.py
@@ -1,6 +1,6 @@
 import os
 from dotenv import load_dotenv
-# from weather import get_weather
+from weather import get_weather
 import colourpalette as cpt
 from datetime import datetime
 import numpy as np
@@ -16,34 +16,30 @@ def main(config):
     curve_config = config["hilbertCurveProperties"]
     weather_config = config['weatherDataProperties']
 
-    load_dotenv()  # dev tool: replaces config.json keys with env keys
+    load_dotenv()  # if .env is used, overwrite config.json with API key and secret
     if os.environ["DATAHUB_API_KEY"] and os.environ["DATAHUB_SECRET"]:
         weather_config["dataHubAPIKey"] = os.getenv("DATAHUB_API_KEY")
         weather_config["dataHubSecret"] = os.getenv("DATAHUB_SECRET")
 
     # Fetch weather data from Met Office DataHub API
-    # input_data: dict = get_weather(weather_config)
+    input_data: dict = get_weather(weather_config)
 
     # Generate Hilbert curve segments
     n = 2
-    # p = cpt.get_p_from_windspeed(input_data['WindSpeed'])
-    p = cpt.get_p_from_windspeed(5)
+    p = cpt.get_p_from_windspeed(input_data['WindSpeed'])
     hilbert_curve = HilbertCurve(p, n, n_procs=curve_config['processCount'])
     distances = np.arange(2**(p*n))
     points = np.asarray(hilbert_curve.points_from_distances(distances))
 
     # Generate colour range (line)
-    # start_line_colour = Color(cpt.line_temperature(round(input_data["MaxTemperature"])))
-    # end_line_colour = Color(cpt.line_temperature(round(input_data["MinTemperature"])))
-    start_line_colour = Color(cpt.line_temperature(19))
-    end_line_colour = Color(cpt.line_temperature(12))
+    start_line_colour = Color(cpt.line_temperature(round(input_data["MinTemperature"])))
+    end_line_colour = Color(cpt.line_temperature(round(input_data["MaxTemperature"])))
     line_colour_range = np.fromiter(start_line_colour.range_to(end_line_colour, len(distances)),
                                     dtype='S16',
                                     count=len(distances))
 
     # Generate colormap (background)
-    # bg_colormap = ListedColormap(cpt.bg_color_list(input_data['SignificantWeatherCode']))
-    bg_colormap = ListedColormap(cpt.bg_color_list('2'))
+    bg_colormap = ListedColormap(cpt.bg_color_list(input_data['SignificantWeatherCode']))
 
     # Configure subplot, background formatting
     # | Removes all axis labels, forces 1:1 aspect ratio, sets size
@@ -51,7 +47,12 @@ def main(config):
     sizes = curve_config["imageDimensions"]
     fig = plt.figure(figsize=(sizes["width"] * px, sizes["height"] * px),
                      dpi=curve_config["displayDPI"])
-    plt.subplots_adjust(left=0, bottom=0, right=1, top=1, wspace=0, hspace=0)
+    plt.subplots_adjust(left=0,
+                        bottom=0,
+                        right=1,
+                        top=1,
+                        wspace=0,
+                        hspace=0)
     ax = fig.add_subplot()
     ax.set_aspect('auto', adjustable='box')
     plt.tick_params(left=False,
@@ -75,12 +76,13 @@ def main(config):
             break
 
     # apply background colormap
-    # bg_shape = cpt.bg_direction(input_data["WindDirection"])
-    bg_shape = cpt.bg_direction(112)
+    bg_shape = cpt.bg_direction(input_data["WindDirection"])
     ax.imshow(bg_shape,
               cmap=bg_colormap,
               interpolation='bicubic',
-              extent=plt.xlim() + plt.ylim(), vmin=0, vmax=1)
+              extent=plt.xlim() + plt.ylim(),
+              vmin=0,
+              vmax=1)
 
     # save figure to file
     if curve_config["saveFigToFile"]:
