bf3f75e5dc226a134ab292322fa2d664cd5e9a9e
corndogit <nathanfinlaybrenton@gmail.com>
2022-06-16 12:21:35 +0100

    add: Pillow implemented for image processing, config options added

--- DataSpaceArt_main/main.py
+++ DataSpaceArt_main/main.py
@@ -1,4 +1,6 @@
 import os
+import io
+from PIL import Image
 from dotenv import load_dotenv
 from weather import get_weather
 import colourpalette as cpt
@@ -21,8 +23,12 @@ def main(config):
         weather_config["dataHubAPIKey"] = os.getenv("DATAHUB_API_KEY")
         weather_config["dataHubSecret"] = os.getenv("DATAHUB_SECRET")
 
-    # Fetch weather data from Met Office DataHub API
+    # Fetch weather data from Met Office DataHub API, optionally printing result
     input_data: dict = get_weather(weather_config)
+    if weather_config['printDataToConsole']:
+        print("Input data log:")
+        for k, v in input_data.items():
+            print(f"{k}: {v}")
 
     # Generate Hilbert curve segments
     n = 2
@@ -84,19 +90,25 @@ def main(config):
               vmin=0,
               vmax=1)
 
-    # save figure to file
+    # store figure image in memory
+    img_bytes = io.BytesIO()
+    file_format: str = curve_config["outputFormat"].lower()  # allowed formats: png, svg, pdf
+    plt.savefig(img_bytes,
+                format=file_format,
+                pad_inches=0)
+
+    img = Image.open(img_bytes)
+    if curve_config['showFigImage']:
+        img.show()
+
+    # optional: save figure to file
     if curve_config["saveFigToFile"]:
         if not os.path.exists('../generated_figures/'):
             os.makedirs('../generated_figures/')
         filename = datetime.now()
-        file_format: str = curve_config["outputFormat"].lower()  # allowed formats: png, svg, pdf
-        plt.savefig(f"../generated_figures/fig-{filename.strftime('%d%m%Y_%H%M%S')}.{file_format}",
-                    format=file_format,
-                    pad_inches=0)
+        img.save(f"../generated_figures/fig-{filename.strftime('%d%m%Y_%H%M%S')}", format=file_format)
 
 
 if __name__ == '__main__':
     with open('../config.json') as config_file:
         main(json.load(config_file))
-
-    plt.show()
